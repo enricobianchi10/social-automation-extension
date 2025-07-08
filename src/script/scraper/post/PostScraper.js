dc47fe7401b15349a372771202e4f4ad0fb3b233
Enrico Bianchi <enrico.bianchi.2002@gmail.com>
2025-07-08 11:49:43 +0200

    sistemato errore se non trova caption

--- src/script/scraper/post/PostScraper.js
+++ src/script/scraper/post/PostScraper.js
@@ -3,7 +3,7 @@ class PostScraper {
         console.log("Inizio scraping del post");
         const post = document.querySelector('img[data-visualcompletion]');
         const postUrl = post?.src || null;
-        const postCaption = document.querySelector(".x1g2khh7 .xzsf02u").innerText; //selettore caption post
+        const postCaption = document.querySelector(".x1g2khh7 .xzsf02u")?.innerText; //selettore caption post
         return new Post(postUrl, postCaption, []);
     }
 }
