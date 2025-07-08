fb4f6353318f61dba354d46afc63f09de666b431
Enrico Bianchi <enrico.bianchi.2002@gmail.com>
2025-07-08 11:47:27 +0200

    sistemato selettore caption post

--- src/script/scraper/post/PostScraper.js
+++ src/script/scraper/post/PostScraper.js
@@ -6,7 +6,7 @@ class PostScraper {
         const post = postList[postList.length - 1];
         const postUrl = post?.src || null;
         // per facebook ma bannato const postCaption = document.querySelector(".x1g2khh7 .xzsf02u").innerText; selettore per caption del post
-        const postCaption = document.querySelector("h1[dir]").innerText; //selettore caption post
+        const postCaption = document.querySelector("div.xt0psk2 h1[dir]").innerText; //selettore caption post
         return new Post(postUrl, postCaption, []);
     }
 }
