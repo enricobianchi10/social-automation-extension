e3a26212fb9cca9ca77f5863b654c65a2a89aaa5
Enrico Bianchi <enrico.bianchi.2002@gmail.com>
2025-07-11 12:22:45 +0200

    tolta visualizzazione dei nuovi post scrapati perchè errata

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -47,14 +47,14 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
             statusContainer.innerHTML = "";
             const statusTitle = document.createElement("h2");
             statusTitle.textContent = "Raccolta dati dai post terminata con successo";
-            const statusDiv = document.createElement("div");
+           /* const statusDiv = document.createElement("div");
             let new_storage = await chrome.storage.local.get(null);
             newPostNumber = Object.keys(new_storage).length;
             statusDiv.innerHTML = `
                 <p>Numero di post raccolti prima: ${oldPostNumber}</p>
                 <p>Nuovi post aggiunti: ${newPostNumber - oldPostNumber}</p>
-                <p>Totale attuale: ${newPostNumber}</p> `;
+                <p>Totale attuale: ${newPostNumber}</p> `; */
             statusContainer.appendChild(statusTitle);
-            statusContainer.appendChild(statusDiv);
+           // statusContainer.appendChild(statusDiv);
     }
 })
