781ee33d609bf698f5c5e58dda477a2d56eb788f
Enrico Bianchi <enrico.bianchi.2002@gmail.com>
2025-07-17 19:21:30 +0200

    Sistemato publish manager per far pubblicare lista di commenti

--- src/script/publisher/publisher_manager.js
+++ src/script/publisher/publisher_manager.js
@@ -1,9 +1,29 @@
-const mockdata = {
-    url: 'https://www.instagram.com/p/DLzs4UaNlqw/',
-    author: 'account_prova1212',
-    text: 'commento 2',
-    replies: 'commento postato automaticamente'
-}
+const mockdatas = [
+    {
+        url: 'https://www.instagram.com/p/DLkPkcLtGy1/',
+        author: 'account_prova1212',
+        text: 'comment 1',
+        replies: 'commento postato automaticamente'
+    },
+    {
+        url: 'https://www.instagram.com/p/DLzs4UaNlqw/',
+        author: 'account_prova1212',
+        text: 'commento 2',
+        replies: 'commento postato automaticamente'
+    },
+    {
+        url: 'https://www.instagram.com/p/DL0GFPrNZYs/',
+        author: 'account_prova1212',
+        text: 'commento nuovo1',
+        replies: 'commento postato automaticamente'
+    },
+    {
+        url: 'https://www.instagram.com/p/DLzs4UaNlqw/',
+        author: 'account_prova1212',
+        text: 'commento 3',
+        replies: 'commento postato automaticamente'
+    }
+];
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     switch (message.action){
@@ -11,7 +31,10 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
             let social = message.social;
             console.log("Ricevuta richiesta di pubblicare un commento");
             await InstagramPageNavigator.goToProfile(social);
-            await InstagramPageNavigator.openLastPost(social);
-            await CommentPublisher.publishRepliesToComment(mockdata.url, mockdata.author, mockdata.text, mockdata.replies, social);
+            for(const mockdata of mockdatas){
+                await InstagramPageNavigator.openLastPost(social);
+                await CommentPublisher.publishRepliesToComment(mockdata.url, mockdata.author, mockdata.text, mockdata.replies, social);
+            }
+            break;
     }
 })
