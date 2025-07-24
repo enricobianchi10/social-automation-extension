a426d99b794bee4cd0a828ccf38a0278645eb603
Enrico Bianchi <enrico.bianchi.2002@gmail.com>
2025-07-24 16:11:48 +0200

    Cambiati selettori per non essere dipendenti dalla lingua impostata

--- src/utils/selectors.js
+++ src/utils/selectors.js
@@ -1,10 +1,11 @@
 const SELECTORS = {
     instagram: {
         commentText: '//article//h3/following-sibling::div[1]/span', //xpath per ottenere il testo dei commenti
-        newCommentsButton: '//article//button[.//*[@aria-label="Carica altri commenti"]]', //xpath per ottenere il pulsante per caricare nuovi commenti
+        newCommentsButton: '//li//button[.//*[name() = "circle"]]', //xpath per ottenere il pulsante per caricare nuovi commenti
         commentAuthor: '//article//h3[not(following-sibling::div[1]/img)]', //xpath per ottenere gli autori dei commenti
-        newPostButton: '//button[.//*[@aria-label="Avanti"]]', //xpath per ottenere il pulsante per andare al prossimo post
-        postImage: '//article//img[@alt and not(ancestor::a) and not(contains(@alt, "profilo"))]', //xpath per ottenere l'immagine del post
+        newPostButton: '//div[contains(@class, "_aaqg")]/button', //per ora tengo così ma soluzione con la classe non mi convince
+        //quello sotto non funziona raggiunta l'ultimo post perchè figlio unico diventa il bottone per tornare indietro e continua avanti-indietro
+        postImage: '//article//div/img[@alt]', //xpath per ottenere l'immagine del post (trova tutte le immagini del carosello, la prima è quella visualizzata)
         postCaption: '//article//h1', //xpath per ottenere la caption del post
         postLikesNumber: '//article//section//a/span[text()]/span', //xpath per ottenere il numero dei like al post
         postNumber: '//li/div/span/span', //xpath per ottenere numero di post
@@ -12,10 +13,18 @@ const SELECTORS = {
         lastPostLink: '//div[1]/div[1]/a[div[2]]', //xpath per trovare ultimo post pubblicato
         //repliesButton: './/span[.//*[contains(text(), "Rispondi")]]', //xpath per trovare il tasto Rispondi al commento (path relativo)
         repliesTextArea: '//article//textarea',
-        publishCommentButton: './/div[contains(text(), "Pubblica")]',
+        publishCommentButton: '//div[preceding-sibling::textarea]/div[@role]', //volendo valido anche '//div/div[@role and text()]'
         boxComment: '//div[h3]'
     }
 }
+
+//xpath vecchi con stringhe in italiano:
+// '//button[.//*[@aria-label="Avanti"]]' vecchio xpath per trovare pulsante nuovo post (cambiato con uno senza controllo valore aria-label dipendente dalla lingua)
+// '//article//button[.//*[@aria-label="Carica altri commenti"]]'
+// '//article//img[@alt and not(ancestor::a) and not(contains(@alt, "profilo"))]'
+// './/div[contains(text(), "Pubblica")]'
+//'//div[count(../div) = 1 or preceding-sibling::div]/button[not(ancestor::ul) and @type]', //xpath per ottenere il pulsante per andare al prossimo post (senza controllo su ul trova anche il pulsante per vedere i taggati e altri, il controllo su @type permette di non selezionare il pulsante per cambiare foto del carosello e quello della foto profilo, il div iniziale ha un controllo e prende solo div che o sono figli unici del div padre o hanno un fratello div precedente, non prende sempre un solo elemento ma il primo è sempre tasto avanti), da problemi perchè quando c'è solo pulsante per tornare indietro trova quello (il find del researcher se l'url che sta cercando non esiste inizierebbe un loop infinito se termino il ciclo indipendentemente da hasNextBtn)
+
 
 //xpath bottone replies '//article//button[.//*[contains(text(), "Visualizza le risposte")]]' //sembra andare anche '//article//div/button'
 //xpath per autori replies '//article//ul/li/ul//div/h3[not(following-sibling::div[1]/img)]'
