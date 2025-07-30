412bf06485ab1ff40a1d1a4a321c7e5e4b6e48b1
Enrico Bianchi <enrico.bianchi.2002@gmail.com>
2025-07-30 19:57:46 +0200

    Piccola modifica per ignorare righe export

--- src/model/Comment.js
+++ src/model/Comment.js
@@ -13,6 +13,8 @@ class Comment {
     }
 }
 
+/* istanbul ignore next */
+// Export for use in Node environment (testing with Jest). Ignored in browsers
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = Comment;
 }
--- src/model/Post.js
+++ src/model/Post.js
@@ -41,6 +41,8 @@ class Post {
     }
 }
 
+/* istanbul ignore next */
+// Export for use in Node environment (testing with Jest). Ignored in browsers
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = Post;
 }
