85d990dec1d843a3fbffe9aa8069fbcbc7d85d92
Enrico Bianchi <enrico.bianchi.2002@gmail.com>
2025-08-29 17:15:24 +0200

    Termine release

--- src/script/publisher/publisher_manager.js
+++ src/script/publisher/publisher_manager.js
@@ -1,30 +1,3 @@
-/* const replies = [
-    {
-        "url": "https://www.instagram.com/p/DLkPkcLtGy1/",
-        "author": "account_prova1212",
-        "text": "comment 1",
-        "replies": "commento postato automaticamente"
-    },
-    {
-        "url": "https://www.instagram.com/p/DLzs4UaNlqw/",
-        "author": "account_prova1212",
-        "text": "commento 2",
-        "replies": "commento postato automaticamente"
-    },
-    {
-        "url": "https://www.instagram.com/p/DLzs4UaNlqw/",
-        "author": "account_prova1212",
-        "text": "commento 3",
-        "replies": "commento postato automaticamente"
-    },
-    {
-        "url": "https://www.instagram.com/p/DL0GFPrNZYs/",
-        "author": "account_prova1212",
-        "text": "commento nuovo1",
-        "replies": "commento postato automaticamente"
-    }
-]; */
-
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     switch (message.action){
         case "publishComment":
